100b8282abd68836bdbd01721debeca6bb54e80f
Fernando Freitas <fsf.freitas@hotmail.com>
2022-04-02 17:31:38 -0300

    Teste

--- gcp-main.tf
+++ gcp-main.tf
@@ -8,16 +8,16 @@ variable "db_password" {
   default = ""
 }
 
-resource "google_sql_database" "g4_database_playlist" {
+resource "google_sql_database" "spotmusic_database_playlist" {
   provider = google
   name     = "playlist"
-  instance = google_sql_database_instance.g4_instance_playlist.name
+  instance = google_sql_database_instance.spotmusic_instance_playlist.name
 }
 
 # See versions at https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/sql_database_instance#database_version
-resource "google_sql_database_instance" "g4_instance_playlist" {
+resource "google_sql_database_instance" "spotmusic_instance_playlist" {
   provider = google
-  name             = "g4-db-playlist"
+  name             = "spotmusic-db-playlist"
   database_version = "MYSQL_8_0"
   settings {
     tier = "db-f1-micro"
@@ -28,7 +28,7 @@ resource "google_sql_database_instance" "g4_instance_playlist" {
 
 resource "google_sql_user" "users" {
   name     = var.db_user
-  instance = google_sql_database_instance.g4_instance_playlist.name
+  instance = google_sql_database_instance.spotmusic_instance_playlist.name
   password = var.db_password
 }
 
@@ -36,7 +36,7 @@ resource "google_artifact_registry_repository" "artifact_playlist" {
   provider = google-beta
   project = "hackathon-6aso-grupo04"
   location = "us-east1"
-  repository_id = "g4-playlist"
+  repository_id = "spotmusic-playlist"
   description = "Imagens Docker"
   format = "DOCKER"
 }
